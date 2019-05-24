8224c57ba122792d855ea8b77b9a0b48e67db127
Mauricio Murga <murga.mauricio@gmail.com>
2019-05-24 12:42:47 -0500

    add fixtures

--- spec/monday_ruby/user_spec.rb
+++ spec/monday_ruby/user_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe MondayRuby::User do
   let(:configuration) { YAML.load_file(file_fixture('configuration.yml')) }
+  let(:users) { JSON.parse(File.open(file_fixture('users.json')).read) }
 
   before do
     MondayRuby.configure do |config|
@@ -8,6 +9,7 @@ RSpec.describe MondayRuby::User do
   end
 
   it 'gets users' do
+    allow_any_instance_of(MondayRuby::Requestor).to receive(:request).and_return(users)
     users = MondayRuby::User.all
 
     expect(users).to be_a(Array)
